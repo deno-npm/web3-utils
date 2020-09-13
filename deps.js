13a26977135151436b2b7c5eaf2d382d31f06efe
Nicolas Guerrero <stephenguerrero43@gmail.com>
2020-09-13 12:21:10 -0500

    Fix import for eth-lib

--- deps.js
+++ deps.js
@@ -7,6 +7,5 @@ export { default as numberToBN } from "https://cdn.skypack.dev/number-to-bn@1.7.
 export { default as randombytes } from "https://cdn.skypack.dev/randombytes@2.1.0";
 export { default as utf8 } from "https://cdn.skypack.dev/utf8@3.0.0";
 export * as _ from "https://cdn.skypack.dev/-/underscore@v1.11.0-8iEGGigleKa9YmBQ2bJF/dist=es2020/underscore.js";
-export * as eth_lib from "https://deno.land/x/npm_eth-lib@0.1.29/mod.js";
-export * as Hash from "https://deno.land/std@0.68.0/hash/mod.ts";
+export * as eth_lib from "https://deno.land/x/npm_eth_lib@0.1.29/mod.js";
 export * as testing from "https://deno.land/std@0.68.0/testing/asserts.ts";
